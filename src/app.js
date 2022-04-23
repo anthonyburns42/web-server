7cba68ee9cf6c75b66e3177437b9ad8b8a8002c8
Anthony Burns <aburns42@gmail.com>
2022-04-23 17:33:07 -0400

    Using express static to serve up html pages.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirPath = path.join(__dirname, '../public');
 
-// Setting up the routes to the different pages in the browser.
-
-// For index page leave blank in quotes.
-app.get('', (req, res) => {
-    // Can send HTML
-    // res.send('<h1>Weather Main Page</h1>');
-    // Can send JSON as well
-    res.send({
-        name: 'Thomas',
-        age: 42
-    });
-});
-
-// For additional pages, /name.
-app.get('/help', (req, res) => {
-    res.send('Help page.');
-});
-
-app.get('/about', (req, res) => {
-    res.send("<h1>About Tony's Weather App</h1>");
-});
+app.use(express.static(publicDirPath));
 
 app.get('/weather', (req, res) => {
     res.send({
