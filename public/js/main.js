c158fce49db899fc0f0056972a5e0dea66412e72
Anthony Burns <aburns42@gmail.com>
2022-04-26 20:44:14 -0400

    Add form to index.hbs and main.js now searching off input query.

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,22 @@
 console.log('Hello, world. Client side.');
 
-// Fetch api works in browser only, won't work in node.js.
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location, data.forecast);
-        }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = search.value;
+    console.log(location);
+
+    // Fetch api works in browser only, won't work in node.js.
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location, data.forecast);
+            }
+        });
     });
 });
