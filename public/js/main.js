948e597c900a245cdf6c20ba934e0293d585151e
Anthony Burns <aburns42@gmail.com>
2022-04-26 21:07:07 -0400

    Added message p tags to index and update the messages through js. Also some css.

--- public/js/main.js
+++ public/js/main.js
@@ -3,19 +3,29 @@ console.log('Hello, world. Client side.');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const msgOne = document.querySelector('#msg-1');
+const msgTwo = document.querySelector('#msg-2');
+
+
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const location = search.value;
     console.log(location);
 
+    msgOne.textContent = 'Loading...';
+    msgTwo.textContent = '';
+
     // Fetch api works in browser only, won't work in node.js.
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
+                msgOne.textContent = data.error;
             } else {
                 console.log(data.location, data.forecast);
+                msgOne.textContent = data.location;
+                msgTwo.textContent = data.forecast;
             }
         });
     });
