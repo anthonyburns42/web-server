09c951a2b71003b161f3a423492c4dc1f7e36e45
Anthony Burns <aburns42@gmail.com>
2022-04-26 20:23:53 -0400

    Started on browser js to fetch data using fetch api.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
 console.log('Hello, world. Client side.');
+
+// Fetch api works in browser only, won't work in node.js.
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location, data.forecast);
+        }
+    });
+});
